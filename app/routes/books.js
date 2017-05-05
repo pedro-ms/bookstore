f0fa27c21294b3a976846d8d90dedce09bb159b8
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-05 14:37:55 -0300

    Create route/books

--- app/routes/books.js
+++ app/routes/books.js
@@ -2,11 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('books');
+    return this.store.findAll('book');
   },
 
   actions{
+    saveBook(newBook) {
+      newBook.save().then(() => this.transitionTo('books'));
+    },
 
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').rollbackAttributes();
+    }
   }
 
 });
