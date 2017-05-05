56e0283f8965befe6ffe55f595db382e9a6234bc
Pedro Souza <pedro-paulo@soaexpert.com.br>
2017-05-05 16:24:18 -0300

    Create routes/books/new

--- app/routes/books/new.js
+++ app/routes/books/new.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function () {
+    return this.store.createRecord('book');
+  },
+
+  actions: {
+    saveBook(newBook) {
+      newBook.save().then(() => this.transitionTo('books'));
+    },
+
+    willTransition() {
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+    }
+  }
 });
