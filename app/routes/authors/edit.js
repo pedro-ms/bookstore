87d2f18c8c40c5515f432d7ceec24a18ccfde54e
Pedro Souza <ppomsg@gmail.com>
2017-05-07 17:38:54 +0000

    Create action save edit

--- app/routes/authors/edit.js
+++ app/routes/authors/edit.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('author', params.author_id);
+  },
+  
+  actions: {
+    saveAuthor(author) {
+      author.save().then(() => this.transitionTo('authors'));
+    },
+
+    willTransition() {
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+    }
+  }
 });
