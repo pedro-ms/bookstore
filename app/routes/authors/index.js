80c8d760de6c0032481c92bb63e0be07d1e5ee7f
Pedro Souza <ppomsg@gmail.com>
2017-05-07 18:14:36 +0000

    Create action delete author

--- app/routes/authors/index.js
+++ app/routes/authors/index.js
@@ -4,6 +4,18 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('author');
+  },
+  
+  actions: {
+    deleteAuthor(author) {
+      let confirmation = confirm('Are you sure?');
+  
+      if (confirmation) {
+        author.destroyRecord();
+      }
+    }
+    
   }
+
 
 });
