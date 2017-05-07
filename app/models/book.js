f42a778d99c709fe9b8b5256ea2f74ceede7a5f9
Pedro Souza <ppomsg@gmail.com>
2017-05-07 15:50:38 +0000

    Add form validations book

--- app/models/book.js
+++ app/models/book.js
@@ -1,8 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   price: DS.attr('number'),
   author: DS.belongsTo('author'),
-  publisher: DS.attr('string')
+  publisher: DS.attr('string'),
+  
+  isValidTitle: Ember.computed.notEmpty('title'),
+  isValidPrice: Ember.computed.match('price', /^\d+(.\d{1,2})?$/),
+  isValidPublisher: Ember.computed.notEmpty('publisher'),
+  isValidAuthor: Ember.computed.notEmpty('author'),
+  isValid: Ember.computed.and('isValidTitle', 'isValidPrice', 'isValidPublisher', 'isValidAuthor')
+
 });
